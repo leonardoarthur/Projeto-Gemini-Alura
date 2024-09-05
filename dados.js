274c31fc194acb1d075c6385183ef2b3abd78af4
Leonardo Arthur <lraguiar.la@gmail.com>
2024-09-05 20:10:51 -0300

    Adição de objetos ao projeto, além da logica em JavaScript.

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Animal Crossing",
+        descricao: "Um jogo de simulação de vida em que você cria sua própria ilha e interage com adoráveis animais.",
+        link: "https://animalcrossing.nintendo.com/"
+    },
+    {
+        titulo: "Fortnite",
+        descricao: "Um jogo de tiro em terceira pessoa com elementos de construção, onde você compete contra outros jogadores para ser o último sobrevivente.",
+        link: "https://www.fortnite.com/"
+    },
+    {
+        titulo: "Minecraft",
+        descricao: "Um jogo de mundo aberto onde você pode construir qualquer coisa que sua imaginação permitir, desde casas simples até castelos gigantes.",
+        link: "https://www.minecraft.net/"
+    },
+    {
+        titulo: "League of Legends",
+        descricao: "Um jogo de arena de batalha multiplayer online (MOBA), onde você escolhe um campeão e trabalha em equipe para destruir a base do inimigo.",
+        link: "https://www.leagueoflegends.com/"
+    }
+];

9c6ed9bd33228448d8d0c67a880bdabdb8e4798a
Leonardo Arthur <lraguiar.la@gmail.com>
2024-09-05 21:11:32 -0300

    Utilização de boas praticas e adição de novas funcionalidades importantes para o código.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log(dados);
+function pesquisar(){
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById('resultados-pesquisa');
+  
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+  
+    // Itera sobre cada item de dados e constrói o HTML para cada resultado
+    for(let dado of dados){
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta"> ${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  
+    // Insere o HTML gerado na seção de resultados
+    section.innerHTML = resultados;
+  }
+
+
